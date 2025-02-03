e5a6bc055753a5f0c462a658be35426fd6e3fab1
Abhinav Randive <arandive27@wooster.edu>
2025-02-03 14:24:46 -0500

    task1

---
+++ 2:3/Array.py
@@ -0,0 +1,16 @@
+
+arr1 = [1, 2, 3, 5, 8]
+arr2 = [1, 5, 7, 3, 5]
+arr3 = []
+
+
+def compare_array(arr1, arr2):
+   for i in range (len(arr1)):
+        for j in range (len(arr2)):
+            if arr1[i] == arr2[j]:
+              arr3.append(arr1[i])
+        
+
+
+compare_array(arr1, arr2)
+print(arr3)
