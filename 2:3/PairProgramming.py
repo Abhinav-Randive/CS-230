f3452d8dc0d59bf35b4759dc631b7a8653178bfc
Abhinav Randive <arandive27@wooster.edu>
2025-02-05 13:26:20 -0500

    Task1

---
+++ 2:3/PairProgramming.py
@@ -0,0 +1,13 @@
+def is_palindrome(word):
+
+    lower_word = word.lower()
+    return lower_word == lower_word[::-1]
+
+word = "Python"
+if is_palindrome(word):
+    bool = True
+    print("Yes, it is a palindrome")
+
+else:
+    bool = False
+    print("No, this is not a palindrome")
---
+++ 2:5/LargVal.py
@@ -0,0 +1,17 @@
+
+int_list = [-5, 3, 7, 1, 22, 17, 4]
+
+# def largest_val(int_list):
+#     return max(int_list)
+
+# print(largest_val(int_list))
+
+# def largest_val(int_list):
+#     for i in range(len(int_list)):
+#         if int_list[i] > int_list[i-1]:
+#             largest = int_list[i]
+#     return largest
+    
+    
+
+# print(largest_val(int_list))
